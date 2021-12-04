8b6524491b012f54ce5fe8dfa0039a08137f3599
Артем Асламов <aslamov@promsoft.ru>
2021-12-04 15:40:38 +0700

    перекинул все файлы из гитлаба

---
+++ frontentend/sl.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import geopandas
+import overpy
+a = geopandas.read_file('moscow.geojson')
+b = a[a.admin_level == "8"][['name', 'geometry']].dropna()
+geopandas.GeoDataFrame.to_file(self=b, filename='dataframe.geojson', driver='GeoJSON')
