3c51f9f637518611325eef066e4d5d53642898c0
Артем Асламов <aslamov@promsoft.ru>
2021-12-04 17:38:34 +0700

    перекинул все файлы из гитлаба

--- frontend/sl.py
+++ frontend/sl.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 import geopandas
 import overpy
-a = geopandas.read_file('moscow.geojson')
-b = a[a.admin_level == "8"][['name', 'geometry']].dropna()
-geopandas.GeoDataFrame.to_file(self=b, filename='dataframe.geojson', driver='GeoJSON')
+#a = geopandas.read_file('moscow.geojson')
+#b = a[a.admin_level == "8"][['name', 'geometry']].dropna()
+#geopandas.GeoDataFrame.to_file(self=b, filename='dataframe.geojson', driver='GeoJSON')
+
+st.write("""
+ifegcbe""")
+
